91a70f3d97c3ee77ea6d0297cdbe4a305bb3228c
Elix Neto <neto.elix@outlook.com>
2024-01-25 09:45:50 -0300

    Fix loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,21 +3,30 @@ import { LoadingContainer } from '../styles/StyleLoading';
 function Loading() {
   return (
     <LoadingContainer>
-      <div>Loading</div>
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
+      <section>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </section>
+      <span>
+        <div>Loading</div>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </span>
     </LoadingContainer>
   );
 }
--- src/styles/StyleLoading.ts
+++ src/styles/StyleLoading.ts
@@ -19,22 +19,37 @@ const loading = keyframes`
 `;
 
 export const LoadingContainer = styled.section`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-content: center;
-  justify-content: center;
-  align-items: center;
-  div {
-    width: 300px;
-    height: 200px;
-    margin: 10px;
-    padding: 20px;
-    box-shadow: 0px 2px 14px rgba(42, 42, 42, 0.24);
-    background-color: #FFFFFF;
-    border-radius: 10px;
-    color: transparent;
-    animation: ${loading} 2s alternate infinite;
-    border: none !important;
-  }
+    display: flex;
+    flex-direction: row-reverse;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    align-items: flex-start;
+    section {
+        border-left: 2px solid #FCA311;
+        padding: 30px;
+    }
+    span {
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 25px;
+    }
+    div {
+        width: 300px;
+        height: 200px;
+        margin: 10px;
+        padding: 20px;
+        box-shadow: 0px 2px 14px rgba(42, 42, 42, 0.24);
+        background-color: #FFFFFF;
+        border-radius: 10px;
+        color: transparent;
+        animation: ${loading} 2s alternate infinite;
+        border: none !important;
+    }
+    @media screen and (max-width: 1000px) {
+        flex-wrap: wrap;
+    }
 `;
