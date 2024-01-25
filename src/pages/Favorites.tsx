009f9f50a392e73017534c85631fb1b2484bc1cf
Elix Neto <neto.elix@outlook.com>
2024-01-25 09:30:01 -0300

    Fix favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,11 +1,28 @@
-import { dataApi } from '../utils/data';
+import { useEffect } from 'react';
+import { ThunkDispatch } from 'redux-thunk';
+import { AnyAction } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
+// import { dataApi } from '../utils/data';
 import { useFavorites } from '../utils/functions';
 import Table from '../components/Table';
 import { FavoriteContainer } from '../styles/StyleFavorites';
+import { fetchData } from '../redux/actions/action';
+import { DataItem, RootState } from '../types';
 
 function Favorites() {
   const { favorites, handleFavorite } = useFavorites();
-  const dataNew = dataApi.data;
+
+  const dispatch: ThunkDispatch<any, any, AnyAction> = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  const dataNew: DataItem[] | null = useSelector(
+    (state: RootState) => state.myReducer.data,
+  );
+
+  // const dataNew = dataApi.data;
   const favoritesData = dataNew.filter((item) => favorites.includes(item.id));
 
   return (
